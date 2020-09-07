c61cc2eacb6f461fc631001e08faa202cc651c95
AlicePolk <55662627+AlicePolk@users.noreply.github.com>
2020-09-07 10:41:44 +0800

    Add files via upload

---
+++ HackLoli/FrmMain.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace HackLoli
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.txtAFile = new System.Windows.Forms.TextBox();
+            this.lblAFile = new System.Windows.Forms.Label();
+            this.txtBFile = new System.Windows.Forms.TextBox();
+            this.lblBFile = new System.Windows.Forms.Label();
+            this.txtCSave = new System.Windows.Forms.TextBox();
+            this.lblCSave = new System.Windows.Forms.Label();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.Location = new System.Drawing.Point(203, 203);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "确定";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMsg.BackColor = System.Drawing.Color.White;
+            this.txtMsg.Location = new System.Drawing.Point(18, 101);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ReadOnly = true;
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMsg.Size = new System.Drawing.Size(260, 96);
+            this.txtMsg.TabIndex = 3;
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // txtAFile
+            // 
+            this.txtAFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAFile.BackColor = System.Drawing.Color.White;
+            this.txtAFile.Location = new System.Drawing.Point(77, 9);
+            this.txtAFile.Name = "txtAFile";
+            this.txtAFile.ReadOnly = true;
+            this.txtAFile.Size = new System.Drawing.Size(201, 21);
+            this.txtAFile.TabIndex = 0;
+            this.txtAFile.Text = " ";
+            this.txtAFile.Click += new System.EventHandler(this.txtFile_Click);
+            // 
+            // lblAFile
+            // 
+            this.lblAFile.AutoSize = true;
+            this.lblAFile.Location = new System.Drawing.Point(18, 12);
+            this.lblAFile.Name = "lblAFile";
+            this.lblAFile.Size = new System.Drawing.Size(53, 12);
+            this.lblAFile.TabIndex = 3;
+            this.lblAFile.Text = "主文件：";
+            // 
+            // txtBFile
+            // 
+            this.txtBFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBFile.BackColor = System.Drawing.Color.White;
+            this.txtBFile.Location = new System.Drawing.Point(77, 37);
+            this.txtBFile.Name = "txtBFile";
+            this.txtBFile.ReadOnly = true;
+            this.txtBFile.Size = new System.Drawing.Size(201, 21);
+            this.txtBFile.TabIndex = 1;
+            this.txtBFile.Click += new System.EventHandler(this.txtFile_Click);
+            // 
+            // lblBFile
+            // 
+            this.lblBFile.AutoSize = true;
+            this.lblBFile.Location = new System.Drawing.Point(18, 40);
+            this.lblBFile.Name = "lblBFile";
+            this.lblBFile.Size = new System.Drawing.Size(53, 12);
+            this.lblBFile.TabIndex = 3;
+            this.lblBFile.Text = "副文件：";
+            // 
+            // txtCSave
+            // 
+            this.txtCSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCSave.BackColor = System.Drawing.Color.White;
+            this.txtCSave.Location = new System.Drawing.Point(77, 68);
+            this.txtCSave.Name = "txtCSave";
+            this.txtCSave.ReadOnly = true;
+            this.txtCSave.Size = new System.Drawing.Size(201, 21);
+            this.txtCSave.TabIndex = 2;
+            this.txtCSave.Click += new System.EventHandler(this.txtCSave_Click);
+            // 
+            // lblCSave
+            // 
+            this.lblCSave.AutoSize = true;
+            this.lblCSave.Location = new System.Drawing.Point(6, 71);
+            this.lblCSave.Name = "lblCSave";
+            this.lblCSave.Size = new System.Drawing.Size(65, 12);
+            this.lblCSave.TabIndex = 3;
+            this.lblCSave.Text = "存储路径：";
+            // 
+            // FrmMain
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 237);
+            this.Controls.Add(this.lblCSave);
+            this.Controls.Add(this.lblBFile);
+            this.Controls.Add(this.lblAFile);
+            this.Controls.Add(this.txtCSave);
+            this.Controls.Add(this.txtBFile);
+            this.Controls.Add(this.txtAFile);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnOk);
+            this.MinimumSize = new System.Drawing.Size(300, 275);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文件捆绑";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.TextBox txtAFile;
+        private System.Windows.Forms.Label lblAFile;
+        private System.Windows.Forms.TextBox txtBFile;
+        private System.Windows.Forms.Label lblBFile;
+        private System.Windows.Forms.TextBox txtCSave;
+        private System.Windows.Forms.Label lblCSave;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+    }
+}
+
---
+++ HackLoli/FrmMain.cs
@@ -0,0 +1,124 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HackLoli
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain() {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 确定按钮开始捆绑文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOk_Click(object sender, EventArgs e) {
+            //保存路径是空则还有必要项没选择
+            if (string.IsNullOrEmpty(txtCSave.Text)) {
+                MessageBox.Show("路径资料填写不完整！");
+            } else {
+                try {
+                    //检查将要新建的文件是否已有重名文件，有则删除
+                    if (File.Exists(txtCSave.Text)) {
+                        File.Delete(txtCSave.Text);
+                    }
+                    string tofileZip = Application.StartupPath + "\\" + Path.GetFileNameWithoutExtension(txtBFile.Text) + ".zip";
+                    FileToZip(txtBFile.Text, tofileZip);
+                    //创建一个进程操作
+                    Process p = new Process();
+                    //设置要启动的应用程序或文档，启动CMD
+                    p.StartInfo.FileName = "cmd.exe";
+                    //不使用操作系统外壳程序启动进程。
+                    p.StartInfo.UseShellExecute = false;
+                    //设置应用程序的输入是从 System.Diagnostics.Process.StandardInput 流中读取。
+                    p.StartInfo.RedirectStandardInput = true;
+                    //设置应用程序的输出写入 System.Diagnostics.Process.StandardOutput 流中。
+                    p.StartInfo.RedirectStandardOutput = true;
+                    //设置应用程序的错误输出写入 System.Diagnostics.Process.StandardError 流中。
+                    p.StartInfo.RedirectStandardError = true;
+                    //在新窗口中启动该进程的值。
+                    p.StartInfo.CreateNoWindow = true;
+                    //启用进程组件
+                    p.Start();
+                    string strOutput = null;
+                    //写入应用程序的流，这里写的是复制捆绑文件的CMD命令
+                    p.StandardInput.WriteLine(string.Format("copy /b {0}+{1} {2}", txtAFile.Text, tofileZip, txtCSave.Text));
+                    //执行完这句后退出CMD
+                    p.StandardInput.WriteLine("exit");
+                    //截获应用程序输出的流
+                    strOutput = p.StandardOutput.ReadToEnd();
+                    //显示在文本框中
+                   // txtMsg.AppendText(strOutput);
+                    //进程操作类开始等待进程退出
+                    p.WaitForExit();
+                    //关闭进程
+                    p.Close();
+                    File.Delete(tofileZip);
+                    MessageBox.Show("捆绑成功！");
+                } catch (Exception ex) {
+                   MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 选择捆绑文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtFile_Click(object sender, EventArgs e) {
+            TextBox txt = (TextBox)sender;
+            if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                txt.Text = openFile.FileName;
+                txt.Tag = Path.GetExtension(openFile.FileName);
+            }
+        }
+
+        //选择保存路径
+        private void txtCSave_Click(object sender, EventArgs e) {
+            //判断是否主文件与副文件已经选择
+            if (txtAFile.Tag != null && txtBFile.Tag != null) {
+                saveFile.Filter = txtAFile.Tag.ToString().Trim('.').ToUpper() + " 文件|*" + txtAFile.Tag + "|" +
+                                txtBFile.Tag.ToString().Trim('.').ToUpper() + " 文件|*" + txtBFile.Tag;
+                if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                    txtCSave.Text = saveFile.FileName;
+                }
+            } else {
+                MessageBox.Show("请先选择有效的主文件和副文件！");
+            }
+        }
+
+        /// <summary>
+        /// 将文件压缩成Zip
+        /// </summary>
+        /// <param name="path">要压缩的文件</param>
+        /// <param name="address">压缩后zip路径</param>
+        private void FileToZip(string path, string address) {
+            string name = Path.GetFileName(path);
+            FileStream StreamToZip = new FileStream(path, FileMode.Open, FileAccess.Read);
+            FileStream ZipFile = File.Create(address);
+            ZipOutputStream ZipStream = new ZipOutputStream(ZipFile);
+            //压缩文件
+            ZipEntry ZipEntry = new ZipEntry(name);
+            ZipStream.PutNextEntry(ZipEntry);
+            ZipStream.SetLevel(6);
+            byte[] buffer = new byte[StreamToZip.Length];
+            StreamToZip.Read(buffer, 0, Convert.ToInt32(StreamToZip.Length));
+            ZipStream.Write(buffer, 0, Convert.ToInt32(StreamToZip.Length));
+            ZipStream.Finish();
+            ZipStream.Close();
+            StreamToZip.Close();
+        }
+
+    }
+}
